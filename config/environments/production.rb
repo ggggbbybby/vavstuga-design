5eb7f7f1b51e4fb91663f4b730cda4bc7b961170
ggggbbybby <rebecca@lendinghome.com>
2017-07-23 23:54:32 -0700

    mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "vavstuga-design_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => 'www.vavstuga.design' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => 'mail.vavstuga.design',
+      :authentication => :plain,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

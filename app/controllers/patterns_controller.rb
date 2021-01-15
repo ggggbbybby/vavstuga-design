7b4fe09d9c390abafb554dbab37b9a60bc48ac75
Rebecca Green <ggggbbybby@gmail.com>
2021-01-15 15:08:56 -0800

    fix no-colors on initialization

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -31,7 +31,9 @@ class PatternsController < ApplicationController
   def create
     @pattern = Pattern.new(pattern_params)
     @pattern.user = current_user || User.first
+    # set empty defaults (why is this necessary now?)
     @pattern.stripes ||= []
+    @pattern.default_colors = {}
 
     respond_to do |format|
       if @pattern.save
